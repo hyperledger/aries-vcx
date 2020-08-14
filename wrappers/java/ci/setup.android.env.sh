1acb584881de4da090bc4077ab3ac2186983a038
Miroslav Kovar <miroslavkovar@protonmail.com>
2020-08-14 11:41:16 +0200

    Minor android build fixes
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- wrappers/java/ci/setup.android.env.sh
+++ wrappers/java/ci/setup.android.env.sh
@@ -287,12 +287,12 @@ copy_libraries_to_jni(){
     TARGET_ARCH=$2
     LIBVCX_DIR=$3
     ANDROID_JNI_LIB="${JAVA_WRAPPER_DIR}/android/src/main/jniLibs"
-    LIB_PATH=${ANDROID_JNI_LIB}/${TARGET_ARCH}
+    LIB_PATH=${ANDROID_JNI_LIB}/${ABI}
     echo "Copying dependencies to ${BOLD}${YELLOW}${LIB_PATH}${RESET}"
     mkdir -p $LIB_PATH
     cp ${LIBVCX_DIR}/target/${TRIPLET}/release/{libvcx.a,libvcx.so} ${LIB_PATH}
+    cp ${INDY_LIB_DIR}/* ${LIB_PATH}
     # cp ${OPENSSL_LIB_DIR}/* ${LIB_PATH}
     # cp ${SODIUM_LIB_DIR}/* ${LIB_PATH}
     # cp ${LIBZMQ_LIB_DIR}/* ${LIB_PATH}
-    # cp ${INDY_LIB_DIR}/* ${LIB_PATH}
 }
