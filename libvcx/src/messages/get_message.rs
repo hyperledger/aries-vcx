121cd943bdcda5a2df64960dec5097c09b3a6170
Miroslav Kovar <miroslavkovar@protonmail.com>
2020-09-25 13:28:27 +0200

    Remove legacy CredentialMessage
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- libvcx/src/messages/get_message.rs
+++ libvcx/src/messages/get_message.rs
@@ -308,23 +308,6 @@ macro_rules! convert_aries_message {
     )
 }
 
-// TODO: Remove?
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub struct CredentialMessage {
-    pub libindy_cred: String,
-    pub rev_reg_def_json: String,
-    pub cred_def_id: String,
-    pub msg_type: String,
-    pub claim_offer_id: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub cred_revoc_id: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub revoc_reg_delta_json: Option<String>,
-    pub version: String,
-    pub from_did: String,
-}
-
-
 impl Message {
     pub fn payload<'a>(&'a self) -> VcxResult<Vec<u8>> {
         match self.payload {
