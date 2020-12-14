8e012e01d4859cee9958dc5a3a1f74598142fb52
Pascal Heitz <pascal.heitz@absa.africa>
2020-12-14 18:38:17 +0100

    using openssl@1.0.2t

--- wrappers/ios/ci/build.sh
+++ wrappers/ios/ci/build.sh
@@ -8,7 +8,7 @@ export RUST_LOG=indy=trace
 export RUST_TEST_THREADS=1
 
 # OpenSSL path changes with version number, so export OPENSSL_DIR=/usr/local/Cellar/openssl/1.0.2n would not work correctly
-OPENSSL_PATH=/usr/local/Cellar/openssl@1.1
+OPENSSL_PATH=/usr/local/Cellar/openssl@1.0.2t
 for i in $(ls -t $OPENSSL_PATH); do
     export OPENSSL_VERSION=$i
     export OPENSSL_DIR=$OPENSSL_PATH/$OPENSSL_VERSION
