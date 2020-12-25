65239461e12db98dc8d88ca775b4912a0912cd1a
Patrik Stas <patrik.stas@absa.africa>
2020-12-25 18:15:55 +0100

    Refactor sending Aries messages
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- libvcx/src/aries/handlers/connection/inviter/state_machine.rs
+++ libvcx/src/aries/handlers/connection/inviter/state_machine.rs
@@ -261,7 +261,7 @@ impl SmConnectionInviter {
                                     .set_explain(err.to_string())
                                     .set_thread_id(&request.id.0);
 
-                                agent_info.send_message(&problem_report.to_a2a_message(), &request.connection.did_doc).ok(); // IS is possible?
+                                request.connection.did_doc.send_message(&problem_report.to_a2a_message(), &agent_info.pw_vk).ok();
                                 InviterState::Null((state, problem_report).into())
                             }
                         }
@@ -292,7 +292,7 @@ impl SmConnectionInviter {
                                 .request_response()
                                 .set_comment(comment);
 
-                        agent_info.send_message(&ping.to_a2a_message(), &state.did_doc).ok();
+                        state.did_doc.send_message(&ping.to_a2a_message(), &agent_info.pw_vk).ok();
                         InviterState::Responded(state)
                     }
                     DidExchangeMessages::PingResponseReceived(ping_response) => {
