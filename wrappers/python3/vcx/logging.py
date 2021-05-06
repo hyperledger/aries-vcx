ab20256f3b3a1fae13cddb2e3637716733444b61
Miroslav Kovar <miroslavkovar@protonmail.com>
2021-05-06 14:05:03 +0200

    Fix VCX python logger initialization
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- wrappers/python3/vcx/logging.py
+++ wrappers/python3/vcx/logging.py
@@ -1,7 +1,7 @@
 from ctypes import *
 
 import logging
-from logging import ERROR, WARNING, INFO, DEBUG, CRITICAL
+from logging import ERROR, WARNING, INFO, DEBUG
 
 TRACE = 5
 
@@ -17,6 +17,7 @@ def set_logger(cdll):
         libvcx_logger = logger.getChild('native.' + target.decode().replace('::', '.'))
 
         level_mapping = {1: ERROR, 2: WARNING, 3: INFO, 4: DEBUG, 5: TRACE, }
+        logging.basicConfig(level=CRITICAL)
 
         libvcx_logger.log(level_mapping[level],
                           "\t%s:%d | %s",
@@ -30,9 +31,12 @@ def set_logger(cdll):
         'flush_cb': None
     }
 
-    getattr(cdll, 'vcx_set_logger')(None,
-                                    set_logger.callbacks['enabled_cb'],
-                                    set_logger.callbacks['log_cb'],
-                                    set_logger.callbacks['flush_cb'])
+    level_mapping = {ERROR: 1, WARNING: 2, INFO: 3, DEBUG: 4, TRACE: 5}
+    c_level = c_uint32(level_mapping.get(logging.root.level) or 0)
 
+    getattr(cdll, 'vcx_set_logger_with_max_lvl')(None,
+                                                 set_logger.callbacks['enabled_cb'],
+                                                 set_logger.callbacks['log_cb'],
+                                                 set_logger.callbacks['flush_cb'],
+                                                 c_level)
     logger.debug("set_logger: <<<")
