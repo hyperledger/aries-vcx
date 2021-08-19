ec56c326f657234b0d8dfe850ffd88dd6628f0cc
Patrik Stas <patrik.stas@absa.africa>
2021-08-19 12:51:49 +0200

    Remove unmaintained python wrapper (#335)
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- wrappers/python3/vcx/api/vcx_init.py
+++
@@ -1,7 +0,0 @@
-from ctypes import *
-import logging
-from vcx.common import do_call, do_call_sync, create_cb
-
-__all__ = []
-
-# this file should contains python init functions delegating initialization to aries-vcx via FFI
