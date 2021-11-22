336530e4b55b1c070058358cfabd2aa10e11994e
kukgini <kukgini@gmail.com>
2021-11-22 19:16:01 +0900

    fix libzmq build error with disable some features (perf and curve-keygen). (#381)
    Signed-off-by: kukgini <kukgini@gmail.com>

--- wrappers/ios/ci/build.sh
+++ wrappers/ios/ci/build.sh
@@ -109,7 +109,8 @@ build_libzmq() {
     popd
 
     pushd $OUTPUT_DIR/libzmq-ios
-        git apply $SCRIPT_DIR/patches/libzmq.rb.patch
+        git checkout -- libzmq.rb
+        git apply --ignore-space-change --ignore-whitespace $SCRIPT_DIR/patches/libzmq.rb.patch
         ./libzmq.rb
     popd
 }
