8bb87280462925d2f570d98ec33346e7f10459d1
Miroslav Kovar <miroslavkovar@protonmail.com>
2022-02-03 12:13:31 +0100

    Add send connection reuse message to vcxagent-core OOB service
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- agents/node/vcxagent-core/src/services/service-out-of-band.js
+++ agents/node/vcxagent-core/src/services/service-out-of-band.js
@@ -37,9 +37,18 @@ module.exports.createServiceOutOfBand = function createServiceOutOfBand ({ logge
     await saveConnection(connectionId, connection)
   }
 
+  async function reuseConnectionFromOobMsg (connectionId, oobMsg) {
+    const oob = await OutOfBandReceiver.createWithMessage(oobMsg)
+    const connection1 = await loadConnection(connectionId)
+    const connection2 = await oob.connectionExists([connection1])
+    if (!connection2) { throw Error('Connection implied in the OOB message was not established yet') }
+    await connection1.sendHandshakeReuse(await oob.getThreadId())
+  }
+
   return {
     createOobMessageWithService,
     createOobMessageWithDid,
-    createConnectionFromOobMsg
+    createConnectionFromOobMsg,
+    reuseConnectionFromOobMsg
   }
 }
