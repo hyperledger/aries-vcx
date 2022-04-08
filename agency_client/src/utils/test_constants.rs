233d90d1fe0bcbbb8a8dd9720910b11dc980f49a
Miroslav Kovar <miroslav.kovar@absa.africa>
2022-04-08 09:51:33 +0200

    Update vcxagency
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- agency_client/src/utils/test_constants.rs
+++ agency_client/src/utils/test_constants.rs
@@ -5,14 +5,5 @@ pub const AGENCY_CONFIGS_UPDATED: &str = r#"
 pub const DELETE_CONNECTION_DECRYPTED_RESPONSE: &str = r#"{"@type":"did:sov:123456789abcdefghi1234;spec/pairwise/1.0/CONN_STATUS_UPDATED","statusCode":"CS-103"}"#;
 pub const AGENCY_MSG_STATUS_UPDATED_BY_CONNS: &str = r#"
 {
-    "@type": "did:sov:123456789abcdefghi1234;spec/pairwise/1.0/MSG_STATUS_UPDATED_BY_CONNS",
-    "failed": [],
-    "updatedUidsByConns": [
-        {
-            "pairwiseDID": "6FRuB95abcmzz1nURoHyWE",
-            "uids": [
-                "Br4CoNP4TU"
-            ]
-        }
-    ]
+    "@type": "did:sov:123456789abcdefghi1234;spec/pairwise/1.0/MSG_STATUS_UPDATED_BY_CONNS"
 }"#;
