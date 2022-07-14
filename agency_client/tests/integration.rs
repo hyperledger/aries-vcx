aab5ae67d3f7bb791705e15c36de10aa805ef6fd
Patrik Stas <patrik.stas@absa.africa>
2022-07-14 17:34:54 +0200

    Split aries-vcx integration tests into multiple files
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- agency_client/tests/integration.rs
+++
@@ -1,6 +0,0 @@
-#[cfg(test)]
-mod tests {
-    use agency_client::agency_client::AgencyClient;
-
-
-}
--- aries_vcx/tests/utils/mod.rs
+++ aries_vcx/tests/utils/mod.rs
@@ -1,1 +1,3 @@
-pub(crate) mod devsetup_agent;
+pub mod devsetup_agent;
+pub mod test_macros;
+pub mod scenarios;
