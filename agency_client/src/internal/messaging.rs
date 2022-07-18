16faaca981f0582f9066ce4b53f95653230c1924
Patrik Stas <patrik.stas@absa.africa>
2022-07-18 16:17:26 +0200

    Simplify agency-client error handling
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- agency_client/src/internal/messaging.rs
+++ agency_client/src/internal/messaging.rs
@@ -103,9 +103,7 @@ impl AgencyClient {
     pub async fn send_message_to_agency(&self, message: &Client2AgencyMessage, did: &str, verkey: &str) -> AgencyClientResult<Vec<Client2AgencyMessage>> {
         trace!("send_message_to_agency >>> message: ..., did: {}", did);
         let data = self.prepare_message_for_agency(message, &did, verkey).await?;
-        let response = self.post_to_agency(&data)
-            .await
-            .map_err(|err| err.map(AgencyClientErrorKind::InvalidHttpResponse, error_utils::INVALID_HTTP_RESPONSE.message))?;
+        let response = self.post_to_agency(&data).await?;
         self.parse_response_from_agency(&response).await
     }
 }
