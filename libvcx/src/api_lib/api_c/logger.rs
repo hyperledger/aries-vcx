f8bf9ee1072b70ad1f565daa9c5642809399712d
Patrik Stas <patrik.stas@absa.africa>
2022-12-19 09:02:55 +0100

    messages: propagate errors instead of logging them, remove dead code
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Remove dead code
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Delete unnecessary error handling code
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Delete more dead code
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Move messages/src/error to messages/src/utils/error
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Create 'concepts' module in messages crate
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Move files in messages crate to concepts/protocols modules
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Simplify error handling in agency-client
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Refactor aries-vcx error handling
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Build libvcx errors using from_msg
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Typo fix, cleanups, refactoring
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Remove dead err mapping code
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Move serde error mapping
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    wip
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    wip
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Use generated mappings to map LibvcxErrorKind to u32
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Seprate VcxError (aries-vcx) and LibvcxError
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Create LibvcxError which handles ErrorKind -> u32 conversions
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Fix test compilation errors
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    aries-vcx: move error.rs to errors module
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    messages: move error.rs to errors module
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    libvcx: move error handling files o errors module
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    libvcx: rename err mapping files
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    agency-client: move error.rs to error module
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Make error file structure consistent across crates
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Implement vcx_error_c_message
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Add missing error codes to node wrapper
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Remove unused error kinds from aries-vcx
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Remove unused libcx error kinds
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Rename errors, error kinds
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Fix compile errors, fix failing test
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Revert naming convetion to original form <xyz>Error, <xyz>ErrorKind
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Address code review
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    Fix test compilation errors
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- libvcx/src/api_lib/api_c/logger.rs
+++ libvcx/src/api_lib/api_c/logger.rs
@@ -4,6 +4,7 @@ use crate::api_lib::errors::error::{LibvcxError, LibvcxErrorKind, SUCCESS_ERR_CO
 use crate::api_lib::errors::error;
 use crate::api_lib::utils::cstring::CStringUtils;
 use crate::api_lib::utils::current_error::set_current_error_vcx;
+use crate::api_lib::errors::error;
 use crate::api_lib::utils::logger::{
     CVoid, EnabledCB, FlushCB,
     LibvcxDefaultLogger, LibvcxLogger, LogCB, LOGGER_STATE,
