1ebd0ab8c631462236ba464e179d75ee93e14103
Patrik Stas <patrik.stas@absa.africa>
2022-12-25 01:09:23 +0100

    Delete dead code
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- aries_vcx/src/utils/json.rs
+++ aries_vcx/src/utils/json.rs
@@ -8,67 +8,6 @@ use serde_json::Value;
 
 use crate::errors::error::prelude::*;
 
-pub trait KeyMatch {
-    fn matches(&self, key: &String, context: &Vec<String>) -> bool;
-}
-
-impl KeyMatch for String {
-    fn matches(&self, key: &String, _context: &Vec<String>) -> bool {
-        key.eq(self)
-    }
-}
-
-/*
-Rewrites keys in a serde value structor to new mapped values. Returns the remapped value. Leaves
-unmapped keys as they are.
-*/
-pub fn mapped_key_rewrite<T: KeyMatch>(val: Value, remap: &Vec<(T, String)>) -> VcxResult<Value> {
-    let mut context: Vec<String> = Default::default();
-    _mapped_key_rewrite(val, &mut context, remap)
-}
-
-fn _mapped_key_rewrite<T: KeyMatch>(
-    val: Value,
-    context: &mut Vec<String>,
-    remap: &Vec<(T, String)>,
-) -> VcxResult<Value> {
-    if let Value::Object(mut map) = val {
-        let mut keys: Vec<String> = _collect_keys(&map);
-
-        while let Some(k) = keys.pop() {
-            let mut value = map.remove(&k).ok_or_else(|| {
-                warn!("Unexpected key value mutation");
-                AriesVcxError::from_msg(AriesVcxErrorKind::InvalidJson, "Unexpected key value mutation")
-            })?;
-
-            let mut new_k = k;
-            for matcher in remap {
-                if matcher.0.matches(&new_k, context) {
-                    new_k = matcher.1.clone();
-                    break;
-                }
-            }
-
-            context.push(new_k.clone()); // TODO not efficient, should work with references
-            value = _mapped_key_rewrite(value, context, remap)?;
-            context.pop();
-
-            map.insert(new_k, value);
-        }
-        Ok(Value::Object(map))
-    } else {
-        Ok(val)
-    }
-}
-
-fn _collect_keys(map: &Map<String, Value>) -> Vec<String> {
-    let mut rtn: Vec<String> = Default::default();
-    for key in map.keys() {
-        rtn.push(key.clone());
-    }
-    rtn
-}
-
 pub(crate) trait TryGetIndex {
     type Val;
     fn try_get(&self, index: &str) -> Result<Self::Val, AriesVcxError>;
@@ -121,28 +60,5 @@ impl AsTypeOrDeserializationError for &Value {
             AriesVcxErrorKind::InvalidJson,
             format!("Could not deserialize '{}' value as bool", self),
         ))
-    }
-}
-
-#[cfg(test)]
-#[cfg(feature = "general_test")]
-mod unit_tests {
-    use super::*;
-
-    #[test]
-    fn simple() {
-        let simple_map = vec![("d".to_string(), "devin".to_string())];
-        let simple = json!({"d":"d"});
-        let expected = json!({"devin":"d"});
-        let transformed = mapped_key_rewrite(simple, &simple_map).unwrap();
-        assert_eq!(expected, transformed);
-
-        let simple = json!(null);
-        let transformed = mapped_key_rewrite(simple.clone(), &simple_map).unwrap();
-        assert_eq!(simple, transformed);
-
-        let simple = json!("null");
-        let transformed = mapped_key_rewrite(simple.clone(), &simple_map).unwrap();
-        assert_eq!(simple, transformed);
     }
 }
