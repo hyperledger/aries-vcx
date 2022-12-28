3cb3e008fde331a2a4b51946be391a6e85d5e691
Patrik Stas <patrik.stas@absa.africa>
2022-12-28 14:18:08 +0100

    Fix compile errors
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- libvcx/src/api_c/mod.rs
+++ libvcx/src/api_c/mod.rs
@@ -1,10 +1,10 @@
 #[macro_use]
-mod cutils;
-mod agent;
-mod ledger;
-mod logger;
-mod protocols;
-mod types;
-mod utils;
-mod vcx;
-mod wallet;
+pub(crate) mod cutils;
+pub(crate) mod agent;
+pub(crate) mod ledger;
+pub(crate) mod logger;
+pub(crate) mod protocols;
+pub(crate) mod types;
+pub(crate) mod utils;
+pub(crate) mod vcx;
+pub(crate) mod wallet;
--- libvcx/src/api_c/protocols/mod.rs
+++ libvcx/src/api_c/protocols/mod.rs
@@ -1,6 +1,6 @@
-mod credential;
-mod disclosed_proof;
-mod issuer_credential;
-mod mediated_connection;
-mod out_of_band;
-mod proof;
+pub(crate) mod credential;
+pub(crate) mod disclosed_proof;
+pub(crate) mod issuer_credential;
+pub(crate) mod mediated_connection;
+pub(crate) mod out_of_band;
+pub(crate) mod proof;
