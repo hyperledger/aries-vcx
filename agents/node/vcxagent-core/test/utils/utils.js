aa7210a35427344d742acbeca5a5e7d17fb5e81b
Miroslav Kovar <miroslav.kovar@absa.africa>
2023-01-03 21:18:18 +0100

    Deduplicate server-related code in agent utils
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- agents/node/vcxagent-core/test/utils/utils.js
+++ agents/node/vcxagent-core/test/utils/utils.js
@@ -5,13 +5,13 @@ const { createFaber } = require('./faber')
 const { createAlice } = require('./alice')
 const { ConnectionStateType } = require('@hyperledger/node-vcx-wrapper')
 
-module.exports.createAliceAndFaber = async function createAliceAndFaber () {
+module.exports.createAliceAndFaber = async function createAliceAndFaber() {
   const alice = await createAlice()
   const faber = await createFaber()
   return { alice, faber }
 }
 
-module.exports.createPairedAliceAndFaber = async function createPairedAliceAndFaber () {
+module.exports.createPairedAliceAndFaber = async function createPairedAliceAndFaber() {
   const alice = await createAlice()
   const faber = await createFaber()
   const invite = await faber.createInvite()
@@ -22,7 +22,7 @@ module.exports.createPairedAliceAndFaber = async function createPairedAliceAndFa
   return { alice, faber }
 }
 
-module.exports.createPairedAliceAndFaberViaPublicInvite = async function createPairedAliceAndFaberViaPublicInvite () {
+async function executeFunctionWithServer(f1, f2) {
   let server
   try {
     const port = 5419
@@ -38,16 +38,9 @@ module.exports.createPairedAliceAndFaberViaPublicInvite = async function createP
     })
     server = app.listen(port)
 
-    const alice = await createAlice()
-    const faber = await createFaber()
-    const pwInfo = await faber.publishService(endpoint)
-    const invite = await faber.createPublicInvite()
-    await alice.acceptInvite(invite)
+    const { alice, faber, pwInfo } = await f1(endpoint)
     const { message } = await faber.unpackMsg(encryptedMsg)
-    await faber.createConnectionFromReceivedRequestV2(pwInfo, message)
-    await alice.updateConnection(ConnectionStateType.Finished)
-    await faber.updateConnection(ConnectionStateType.Finished)
-    return { alice, faber }
+    return await f2(alice, faber, pwInfo, message)
   } catch (err) {
     console.error(`err = ${err.message} stack = ${err.stack}`)
     await sleep(2000)
@@ -60,77 +53,55 @@ module.exports.createPairedAliceAndFaberViaPublicInvite = async function createP
   }
 }
 
-module.exports.createPairedAliceAndFaberViaOobMsg = async function createPairedAliceAndFaberViaOobMsg () {
-  let server
-  try {
-    const port = 5420
-    const path = '/msg'
-    const endpoint = `http://127.0.0.1:${port}${path}`
+module.exports.createPairedAliceAndFaberViaPublicInvite = async function createPairedAliceAndFaberViaPublicInvite() {
+  const f1 = async (endpoint) => {
+    const alice = await createAlice()
+    const faber = await createFaber()
+    const pwInfo = await faber.publishService(endpoint)
+    const invite = await faber.createPublicInvite()
+    await alice.acceptInvite(invite)
+    return { alice, faber, pwInfo }
+  }
+  const f2 = async (alice, faber, pwInfo, message) => {
+    await faber.createConnectionFromReceivedRequestV2(pwInfo, message)
+    await alice.updateConnection(ConnectionStateType.Finished)
+    await faber.updateConnection(ConnectionStateType.Finished)
+    return { alice, faber }
+  }
+  return await executeFunctionWithServer(f1, f2)
+}
 
-    let encryptedMsg
-    const app = express()
-    app.use(bodyParser.raw({ type: '*/*' }))
-    app.post(path, (req, res) => {
-      encryptedMsg = req.body
-      res.status(200).send()
-    })
-    server = app.listen(port)
-
+module.exports.createPairedAliceAndFaberViaOobMsg = async function createPairedAliceAndFaberViaOobMsg() {
+  const f1 = async (endpoint) => {
     const alice = await createAlice()
     const faber = await createFaber()
     const pwInfo = await faber.publishService(endpoint)
     const msg = await faber.createOobMessageWithDid()
     await alice.createConnectionUsingOobMessage(msg)
     await alice.updateConnection(ConnectionStateType.Requested)
-    const { message } = await faber.unpackMsg(encryptedMsg)
+    return { alice, faber, pwInfo }
+  }
+  const f2 = async (alice, faber, pwInfo, message) => {
     await faber.createConnectionFromReceivedRequestV2(pwInfo, message)
     await alice.updateConnection(ConnectionStateType.Finished)
     await faber.updateConnection(ConnectionStateType.Finished)
     return { alice, faber }
-  } catch (err) {
-    console.error(`err = ${err.message} stack = ${err.stack}`)
-    await sleep(2000)
-    throw Error(err)
-  } finally {
-    if (server) {
-      server.close()
-      await sleep(3000)
-    }
   }
+  return await executeFunctionWithServer(f1, f2)
 }
 
-module.exports.connectViaOobMessage = async function connectViaOobMessage (alice, faber, msg) {
-  let server
-  try {
-    const port = 5421
-    const path = '/msg'
-    const endpoint = `http://127.0.0.1:${port}${path}`
-
-    let encryptedMsg
-    const app = express()
-    app.use(bodyParser.raw({ type: '*/*' }))
-    app.post(path, (req, res) => {
-      encryptedMsg = req.body
-      res.status(200).send()
-    })
-    server = app.listen(port)
-
+module.exports.connectViaOobMessage = async function connectViaOobMessage(alice, faber, msg) {
+  const f1 = async (endpoint) => {
     const pwInfo = await faber.publishService(endpoint)
     await alice.createConnectionUsingOobMessage(msg)
     await alice.updateConnection(ConnectionStateType.Requested)
-    const { message } = await faber.unpackMsg(encryptedMsg)
+    return { alice, faber, pwInfo }
+  }
+  const f2 = async (alice, faber, pwInfo, message) => {
     await faber.createConnectionFromReceivedRequestV2(pwInfo, message)
     await alice.updateConnection(ConnectionStateType.Finished)
     await faber.updateConnection(ConnectionStateType.Finished)
     return { alice, faber }
-  } catch (err) {
-    console.error(`err = ${err.message} stack = ${err.stack}`)
-    await sleep(2000)
-    throw Error(err)
-  } finally {
-    if (server) {
-      server.close()
-      await sleep(3000)
-    }
   }
+  return await executeFunctionWithServer(f1, f2)
 }
