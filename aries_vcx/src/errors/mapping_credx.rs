d9915dfb8b5169da0b9c00c1448ad09fe68b4564
gmulhearn <gmulhearn@proton.me>
2023-01-12 18:37:54 +1000

    improvement to check
    Signed-off-by: gmulhearn <gmulhearn@proton.me>

--- aries_vcx/src/errors/mapping_credx.rs
+++ aries_vcx/src/errors/mapping_credx.rs
@@ -4,8 +4,12 @@ use indy_credx::Error as CredxError;
 impl From<CredxError> for AriesVcxError {
     fn from(err: CredxError) -> Self {
         // Credx will occasionally wrap the real error within the `cause` of an ErrorKind::Input error type
-        // So we extract it if the cause exists and can be downcast to an credxerror
-        let cause = &err.cause.as_ref().and_then(|x| x.downcast_ref::<CredxError>());
+        // So we use this cause error if the cause exists and can be downcast to an credxerror
+        let cause = if err.kind() == indy_credx::ErrorKind::Input {
+            err.cause.as_ref().and_then(|x| x.downcast_ref::<CredxError>())
+        } else {
+            None
+        };
         let e = cause.unwrap_or(&err);
 
         match e.kind() {
