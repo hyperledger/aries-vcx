d4192ce564742f1bbc1539edfff2bde9b31ef3a1
Patrik Stas <patrik.stas@absa.africa>
2023-01-12 23:17:12 +0100

    vdrtools: Remove unused code
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- libvdrtools/tests/utils/rand_utils.rs
+++
@@ -1,9 +0,0 @@
-use rand::{distributions::Alphanumeric, Rng};
-
-pub fn get_rand_string(len: usize) -> String {
-    rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(len)
-        .map(char::from)
-        .collect()
-}
