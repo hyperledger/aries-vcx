26a2441ff505d8ffb56b02e1f6c8dc529ab79afa
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-02-07 14:18:45 +0200

    Added re-export of PairwiseInfo to mediatedconnection to avoid struct duplication
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- aries_vcx/src/protocols/mediated_connection/pairwise_info.rs
+++ aries_vcx/src/protocols/mediated_connection/pairwise_info.rs
@@ -1,17 +1,2 @@
-use std::sync::Arc;
-
-use crate::errors::error::VcxResult;
-use crate::plugins::wallet::base_wallet::BaseWallet;
-
-#[derive(Default, Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub struct PairwiseInfo {
-    pub pw_did: String,
-    pub pw_vk: String,
-}
-
-impl PairwiseInfo {
-    pub async fn create(wallet: &Arc<dyn BaseWallet>) -> VcxResult<PairwiseInfo> {
-        let (pw_did, pw_vk) = wallet.create_and_store_my_did(None, None).await?;
-        Ok(PairwiseInfo { pw_did, pw_vk })
-    }
-}
+/// Making it easy to remove this in the future without duplicating the struct.
+pub use crate::protocols::connection::pairwise_info::PairwiseInfo;
