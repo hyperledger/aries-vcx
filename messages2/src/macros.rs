7265d52bf106298d99dc9adbfe5441aef8f47fc2
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-02-15 12:44:43 +0200

    Added comments to A2AMessage Serialize impl
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- messages2/src/macros.rs
+++ messages2/src/macros.rs
@@ -1,7 +1,7 @@
 /// Macro for providing upstream [`From`] implementations.
 macro_rules! transient_from {
-    // Implement [`From`] [`$source`] for [`$target`]
-    // if [`From`] [`$interm`] for [`$target`] is implemented.
+    // Implement [`From`] `$source` for `$target`
+    // if [`From`] `$interm` for `$target` is implemented.
     ($source:ident, $interm:ident, $target:ident) => {
         impl From<$source> for $target {
             fn from(value: $source) -> $target {
@@ -11,9 +11,9 @@ macro_rules! transient_from {
         }
     };
 
-    // Implement [`From`] [`$first`] for [`$target`]
-    // and [`From`] [`$source`] for [`$target`]
-    // if [`From`] [`$second`] for [`$target`] is implemented.
+    // Implement [`From`] `$first` for `$target`
+    // and [`From`] `$source` for `$target`
+    // if [`From`] `$second` for `$target` is implemented.
     ($source:ident, $first:ident, $second:ident, $target:ident) => {
         impl From<$first> for $target {
             fn from(value: $first) -> $target {
