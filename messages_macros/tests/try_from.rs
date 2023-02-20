dcaa06f11dcc229c4c4be5b56b87abd928e175c8
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-02-20 09:30:52 +0200

    Added try_from test and comments
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- messages_macros/tests/try_from.rs
+++ messages_macros/tests/try_from.rs
@@ -1,55 +1,80 @@
 #![allow(unused_variables)]
+#![allow(unused_must_use)]
 
-use messages_macros::TransitiveFrom;
+use messages_macros::TransitiveTryFrom;
 
 #[test]
 fn try_from() {
-    #[derive(TransitiveFrom)]
-    #[transitive(B, C, D, E, F)] // impl From<A> for F
+    #[derive(TransitiveTryFrom)]
+    #[transitive(B, C, D, E, F, G)] // impl TryFrom<G> for A
     struct A;
-    #[derive(TransitiveFrom)]
-    #[transitive(C, D, E)] // impl From<B> for E
-    #[transitive(E, F)] // impl From<B> for F => Since we already implement B -> E above, this works!
+    #[derive(TransitiveTryFrom)]
+    #[transitive_all(C, D, E, F, G)] // impl TryFrom<G> for E, D, C and B
     struct B;
+    #[derive(TransitiveTryFrom)]
+    #[transitive(D, E, F)] // impl TryFrom<F> for C
+    #[transitive(F, G)] // impl TryFrom<G> for C => Since we already implement C -> F above, this works!
     struct C;
     struct D;
     struct E;
     struct F;
+    struct G;
 
-    impl From<A> for B {
-        fn from(val: A) -> B {
-            B
+    impl TryFrom<G> for F {
+        type Error = ();
+
+        fn try_from(val: G) -> Result<Self, Self::Error> {
+            Ok(F)
         }
     }
 
-    impl From<B> for C {
-        fn from(val: B) -> C {
-            C
+    impl TryFrom<F> for E {
+        type Error = ();
+
+        fn try_from(val: F) -> Result<Self, Self::Error> {
+            Ok(E)
         }
     }
 
-    impl From<C> for D {
-        fn from(val: C) -> D {
-            D
+    impl TryFrom<E> for D {
+        type Error = ();
+
+        fn try_from(val: E) -> Result<Self, Self::Error> {
+            Ok(D)
         }
     }
 
-    impl From<D> for E {
-        fn from(val: D) -> E {
-            E
+    impl TryFrom<D> for C {
+        type Error = ();
+
+        fn try_from(val: D) -> Result<Self, Self::Error> {
+            Ok(C)
         }
     }
 
-    impl From<E> for F {
-        fn from(val: E) -> F {
-            F
+    impl TryFrom<C> for B {
+        type Error = ();
+
+        fn try_from(val: C) -> Result<Self, Self::Error> {
+            Ok(B)
         }
     }
 
-    let a = A;
-    let f = F::from(a);
+    impl TryFrom<B> for A {
+        type Error = ();
 
-    let b = B;
-    let e = E::from(b);
-    let f = F::from(B);
+        fn try_from(val: B) -> Result<Self, Self::Error> {
+            Ok(A)
+        }
+    }
+
+    A::try_from(G);
+
+    B::try_from(D);
+    B::try_from(E);
+    B::try_from(F);
+    B::try_from(G);
+
+    C::try_from(F);
+    C::try_from(G);
 }
