b5132385fd83cbdf8196075d7172f69031c5ba39
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-02-21 22:46:25 +0200

    Added draft for Forward message
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- messages2/src/protocols/routing.rs
+++ messages2/src/protocols/routing.rs
@@ -1,5 +1,6 @@
 use messages_macros::Message;
 use serde::{Deserialize, Serialize};
+use serde_json::value::RawValue;
 
 use crate::message_type::message_family::routing::{Routing, RoutingV1, RoutingV1_0};
 
@@ -7,4 +8,10 @@ use super::traits::ConcreteMessage;
 
 #[derive(Clone, Debug, Deserialize, Serialize, Message)]
 #[message(kind = "Routing::V1(RoutingV1::V1_0(RoutingV1_0::Forward))")]
-pub struct Forward;
+pub struct Forward {
+    #[serde(rename = "@id")]
+    pub id: String,
+    pub to: String,
+    #[serde(rename = "msg")]
+    pub msg: Box<RawValue>,
+}
