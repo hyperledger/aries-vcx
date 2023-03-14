44057f85a11c1cb2fb83f5ff42f2a32570561492
Patrik Stas <patrik.stas@absa.africa>
2023-03-14 18:22:15 +0100

    Reformat
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- aries_vcx/tests/test_creds_proofs_revocations.rs
+++ aries_vcx/tests/test_creds_proofs_revocations.rs
@@ -12,7 +12,7 @@ mod integration_tests {
     use std::time::Duration;
 
     use aries_vcx::protocols::proof_presentation::prover::state_machine::ProverState;
-    use aries_vcx::protocols::proof_presentation::verifier::state_machine::{VerifierState};
+    use aries_vcx::protocols::proof_presentation::verifier::state_machine::VerifierState;
     use aries_vcx::protocols::proof_presentation::verifier::verification_status::PresentationVerificationStatus;
     use aries_vcx::utils::devsetup::*;
 
