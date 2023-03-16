6f2e4948a53e90a073850f8d4df336eb03d7c539
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-03-16 17:47:19 +0200

    Added TrustPing msgs tests
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- messages2/src/protocols/trust_ping/ping_response.rs
+++ messages2/src/protocols/trust_ping/ping_response.rs
@@ -30,3 +30,63 @@ impl PingResponseDecorators {
         Self { thread, timing: None }
     }
 }
+
+#[cfg(test)]
+#[allow(clippy::unwrap_used)]
+#[allow(clippy::field_reassign_with_default)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+    use crate::misc::test_utils;
+
+    #[test]
+    fn test_minimal_message() {
+        let msg_type = test_utils::build_msg_type::<PingResponseContent>();
+
+        let content = PingResponseContent::default();
+
+        let thid = "test".to_owned();
+        let thread = Thread::new(thid.clone());
+        let decorators = PingResponseDecorators::new(thread);
+
+        let json = json!({
+            "@type": msg_type,
+            "~thread": {
+                "thid": thid
+            }
+        });
+
+        test_utils::test_msg(content, decorators, json);
+    }
+
+    #[test]
+    fn test_extensive_message() {
+        let msg_type = test_utils::build_msg_type::<PingResponseContent>();
+
+        let mut content = PingResponseContent::default();
+        let comment_str = "test".to_owned();
+        content.comment = Some(comment_str.clone());
+
+        let thid = "test".to_owned();
+        let thread = Thread::new(thid.clone());
+        let mut decorators = PingResponseDecorators::new(thread);
+        let in_time = "test".to_owned();
+        let mut timing = Timing::default();
+        timing.in_time = Some(in_time.clone());
+        decorators.timing = Some(timing);
+
+        let json = json!({
+            "@type": msg_type,
+            "comment": comment_str,
+            "~thread": {
+                "thid": thid
+            },
+            "~timing": {
+                "in_time": in_time
+            }
+        });
+
+        test_utils::test_msg(content, decorators, json);
+    }
+}
