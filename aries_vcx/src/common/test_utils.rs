2f7da94e22fee60eac6968d3e71e04a774b9e1b1
Styvane Soukossi <4968379+styvane@users.noreply.github.com>
2023-03-18 16:15:55 +0100

    ref(776): refactor `encode_attributes` function
    Signed-off-by: Styvane Soukossi <4968379+styvane@users.noreply.github.com>

--- aries_vcx/src/common/test_utils.rs
+++ aries_vcx/src/common/test_utils.rs
@@ -7,7 +7,7 @@ use std::time::Duration;
 use aries_vcx_core::PoolHandle;
 use aries_vcx_core::WalletHandle;
 
-use crate::common::credentials::encoding::encode_attributes;
+use crate::common::credentials::encoding::{CredentialAttributeData, EncodedCredentialAttributes};
 use crate::common::primitives::credential_definition::CredentialDef;
 use crate::common::primitives::credential_definition::CredentialDefConfigBuilder;
 use crate::common::primitives::revocation_registry::RevocationRegistry;
@@ -177,7 +177,9 @@ pub async fn create_and_store_credential(
 
     /* create cred */
     let credential_data = r#"{"address1": ["123 Main St"], "address2": ["Suite 3"], "city": ["Draper"], "state": ["UT"], "zip": ["84000"]}"#;
-    let encoded_attributes = encode_attributes(&credential_data).unwrap();
+    let EncodedCredentialAttributes {
+        data: encoded_attributes,
+    } = CredentialAttributeData::new(credential_data).encode().unwrap();
     let ledger = Arc::clone(issuer).inject_ledger();
     let rev_def_json = ledger.get_rev_reg_def_json(&rev_reg_id).await.unwrap();
     let tails_file = get_temp_dir_path(TAILS_DIR).to_str().unwrap().to_string();
@@ -229,7 +231,9 @@ pub async fn create_and_store_nonrevocable_credential(
 
     /* create cred */
     let credential_data = r#"{"address1": ["123 Main St"], "address2": ["Suite 3"], "city": ["Draper"], "state": ["UT"], "zip": ["84000"]}"#;
-    let encoded_attributes = encode_attributes(&credential_data).unwrap();
+    let EncodedCredentialAttributes {
+        data: encoded_attributes,
+    } = CredentialAttributeData::new(&credential_data).encode().unwrap();
 
     let (cred, _, _) = Arc::clone(issuer)
         .inject_anoncreds()
