63f4472b6efdff5c368bd5fe61299147796138ed
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-04-07 12:50:56 +0300

    With teary eyes added #[serde(default)] to PleaseAck decorator for backwards compatibility
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- messages/src/decorators/please_ack.rs
+++ messages/src/decorators/please_ack.rs
@@ -3,6 +3,17 @@ use serde::{Deserialize, Serialize};
 /// Struct representing the `~please_ack` decorators from its [RFC](<https://github.com/hyperledger/aries-rfcs/blob/main/features/0317-please-ack/README.md>).
 #[derive(Debug, Default, Clone, Serialize, Deserialize, PartialEq)]
 pub struct PleaseAck {
+    // This is wrong, but necessary for backwards compatibility.
+    // Per the [RFC](<https://github.com/hyperledger/aries-rfcs/blob/main/features/0317-please-ack/README.md#on>)
+    // this is a *required* array.
+    //
+    // However, the entire field was previously NOT serialized if it was empty,
+    // resulting in something like '"~please_ack": null' instead of '"~please_ack": {"on": []}'.
+    //
+    // One could argue that the field could be treated even better, so that an empty array would be incorrect.
+    // Perhaps using an enum altogether (if the values become somewhat stable) or ordering the array might be of interest,
+    // so that processing happens in a specific order.
+    #[serde(default)]
     pub on: Vec<AckOn>,
 }
 
