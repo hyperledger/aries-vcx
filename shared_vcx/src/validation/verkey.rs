382184455c09db056796ad4f683ba6ac5e046bf2
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-04-14 12:50:53 +0300

    Removed feature flags from shared_vcx
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- shared_vcx/src/validation/verkey.rs
+++ shared_vcx/src/validation/verkey.rs
@@ -11,7 +11,7 @@ pub fn validate_verkey(verkey: &str) -> SharedVcxResult<String> {
         )),
         Err(err) => Err(SharedVcxError::from_msg(
             SharedVcxErrorKind::NotBase58,
-            format!("Verkey is not valid base58, details: {}", err),
+            format!("Verkey is not valid base58, details: {err}"),
         )),
     }
 }
@@ -21,30 +21,27 @@ mod tests {
     use super::*;
 
     #[test]
-    #[cfg(feature = "general_test")]
     fn test_verkey_is_b58_and_valid_length() {
         let verkey = "EkVTa7SCJ5SntpYyX7CSb2pcBhiVGT9kWSagA8a9T69A";
-        match validate_verkey(&verkey) {
+        match validate_verkey(verkey) {
             Err(_) => panic!("Should be valid verkey"),
             Ok(x) => assert_eq!(x, verkey),
         }
     }
 
     #[test]
-    #[cfg(feature = "general_test")]
     fn test_verkey_is_b58_but_invalid_length() {
         let verkey = "8XFh8yBzrpJQmNyZzgoT";
-        match validate_verkey(&verkey) {
+        match validate_verkey(verkey) {
             Err(x) => assert_eq!(x.kind(), SharedVcxErrorKind::InvalidVerkey),
             Ok(_) => panic!("Should be invalid verkey"),
         }
     }
 
     #[test]
-    #[cfg(feature = "general_test")]
     fn test_validate_verkey_with_non_base58() {
         let verkey = "*kVTa7SCJ5SntpYyX7CSb2pcBhiVGT9kWSagA8a9T69A";
-        match validate_verkey(&verkey) {
+        match validate_verkey(verkey) {
             Err(x) => assert_eq!(x.kind(), SharedVcxErrorKind::NotBase58),
             Ok(_) => panic!("Should be invalid verkey"),
         }
