495171ff4c2e5e4b5684df3ade419fa5bce3c261
Bhavy Airi <airiragahv@gmail.com>
2023-04-21 16:22:10 +0530

    Add unit test for SerializableObjectWithState
    Signed-off-by: Bhavy Airi <airiragahv@gmail.com>

--- aries_vcx/src/utils/serialization.rs
+++ aries_vcx/src/utils/serialization.rs
@@ -47,3 +47,53 @@ pub enum SerializableObjectWithState<T, P> {
         thread_id: String,
     },
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde::{Deserialize, Serialize};
+
+    #[test]
+    fn test_serialize() {
+        let value = Schema {
+            data: vec!["name".to_string(), "age".to_string()],
+            state: null,
+            source_id: "foo".to_string(),
+            thread_id: "bat".to_string(),
+            ..Schema::default()
+        };
+
+        let serialized = value.serialize().unwrap();
+        assert!(serialized.contains(r#""data":["name","age"]"#));
+        assert!(serialized.contains(r#""source_id":"foo""#));
+        assert!(serialized.contains(r#""state":"null""#));
+        assert!(serialized.contains(r#""thread":"bat""#));
+
+    }
+
+    #[test]
+    fn test_deserialize() {
+        let serialized = r#"
+        {
+          "data": {
+            "data": [
+              "name",
+              "age"
+            ],
+            "state": 1,
+            "source_id": "foo",
+            "thread_id": "bat"
+          }
+        }
+        "#;
+
+        let result = deserialize(serialized);
+        assert!(result.is_ok());
+        let ans = result.unwrap();
+
+        assert_eq!(ans.data, vec!["name".to_string(), "age".to_string()]);
+        assert_eq!(ans.state, PublicEntityStateType::Published);
+        assert_eq!(ans.source_id, "foo");
+        assert_eq!(ans.thread_id, "bat");
+    }
+}
