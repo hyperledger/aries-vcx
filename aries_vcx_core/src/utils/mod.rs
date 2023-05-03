e380aac4365b1d4c0b352954a5c8e9120a905716
Patrik <patrik.stas@absa.africa>
2023-05-03 12:29:30 +0200

    Fix setting txn author agreement (#824)
    * Fix setting txn author agreement
    
    ---------
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- aries_vcx_core/src/utils/mod.rs
+++ aries_vcx_core/src/utils/mod.rs
@@ -1,7 +1,7 @@
 use std::{env, path::PathBuf};
 
 pub mod async_fn_iterator;
-pub(crate) mod author_agreement;
+pub mod author_agreement;
 pub(crate) mod constants;
 pub(crate) mod json;
 pub(crate) mod mockdata;
