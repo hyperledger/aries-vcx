70e63f21c565980f16e963b9186722882f31835a
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-06-14 16:46:56 +0300

    Added constants for Indy categories
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- cred_migrator/src/vdrtools2credx/mod.rs
+++ cred_migrator/src/vdrtools2credx/mod.rs
@@ -4,27 +4,38 @@ use vdrtools::types::domain::wallet::Record;
 
 use crate::error::MigrationResult;
 
+const INDY_MASTER_SECRET: &str = "Indy::MasterSecret";
+const INDY_CRED: &str = "Indy::Credential";
+const INDY_CRED_DEF: &str = "Indy::CredentialDefinition";
+const INDY_CRED_DEF_PRIV: &str = "Indy::CredentialDefinitionPrivateKey";
+const INDY_CRED_DEF_CR_PROOF: &str = "Indy::CredentialDefinitionCorrectnessProof";
+const INDY_SCHEMA: &str = "Indy::Schema";
+const INDY_SCHEMA_ID: &str = "Indy::SchemaId";
+const INDY_REV_REG: &str = "Indy::RevocationRegistry";
+const INDY_REV_REG_DELTA: &str = "Indy::RevocationRegistryDelta";
+const INDY_REV_REG_INFO: &str = "Indy::RevocationRegistryInfo";
+const INDY_REV_REG_DEF: &str = "Indy::RevocationRegistryDefinition";
+const INDY_REV_REG_DEF_PRIV: &str = "Indy::RevocationRegistryDefinitionPrivate";
+
 /// Contains the logic for record mapping and migration.
 pub fn migrate_any_record(record: Record) -> MigrationResult<Option<Record>> {
     match record.type_.as_str() {
         // Master secret
-        "Indy::MasterSecret" => Some(conv::convert_master_secret(record)).transpose(),
+        INDY_MASTER_SECRET => Some(conv::convert_master_secret(record)).transpose(),
         // Credential
-        "Indy::Credential" => Some(conv::convert_cred(record)).transpose(),
-        "Indy::CredentialDefinition" => Some(conv::convert_cred_def(record)).transpose(),
-        "Indy::CredentialDefinitionPrivateKey" => Some(conv::convert_cred_def_priv_key(record)).transpose(),
-        "Indy::CredentialDefinitionCorrectnessProof" => {
-            Some(conv::convert_cred_def_correctness_proof(record)).transpose()
-        }
+        INDY_CRED => Some(conv::convert_cred(record)).transpose(),
+        INDY_CRED_DEF => Some(conv::convert_cred_def(record)).transpose(),
+        INDY_CRED_DEF_PRIV => Some(conv::convert_cred_def_priv_key(record)).transpose(),
+        INDY_CRED_DEF_CR_PROOF => Some(conv::convert_cred_def_correctness_proof(record)).transpose(),
         // Schema
-        "Indy::Schema" => Some(conv::convert_schema(record)).transpose(),
-        "Indy::SchemaId" => Some(conv::convert_schema_id(record)).transpose(),
+        INDY_SCHEMA => Some(conv::convert_schema(record)).transpose(),
+        INDY_SCHEMA_ID => Some(conv::convert_schema_id(record)).transpose(),
         // Revocation registry
-        "Indy::RevocationRegistry" => Some(conv::convert_rev_reg(record)).transpose(),
-        "Indy::RevocationRegistryDelta" => Some(conv::convert_rev_reg_delta(record)).transpose(),
-        "Indy::RevocationRegistryInfo" => Some(conv::convert_rev_reg_info(record)).transpose(),
-        "Indy::RevocationRegistryDefinition" => Some(conv::convert_rev_reg_def(record)).transpose(),
-        "Indy::RevocationRegistryDefinitionPrivate" => Some(conv::convert_rev_reg_def_priv(record)).transpose(),
+        INDY_REV_REG => Some(conv::convert_rev_reg(record)).transpose(),
+        INDY_REV_REG_DELTA => Some(conv::convert_rev_reg_delta(record)).transpose(),
+        INDY_REV_REG_INFO => Some(conv::convert_rev_reg_info(record)).transpose(),
+        INDY_REV_REG_DEF => Some(conv::convert_rev_reg_def(record)).transpose(),
+        INDY_REV_REG_DEF_PRIV => Some(conv::convert_rev_reg_def_priv(record)).transpose(),
         _ => Ok(None), // Ignore unknown/uninteresting records
     }
 }
