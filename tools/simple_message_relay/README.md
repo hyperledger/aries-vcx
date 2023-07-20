e1414a8c5ea84623831a479808bc7aff60ff2378
George Mulhearn <57472912+gmulhearn@users.noreply.github.com>
2023-07-20 18:31:23 +1000

    Partially #870 - Simple Message Relay for testing/demo purposes (#891)
    Partially #870 - Simple Message Relay for testing/demo purposes (#891)
    
    Signed-off-by: gmulhearn <gmulhearn@proton.me>

---
+++ tools/simple_message_relay/README.md
@@ -0,0 +1,64 @@
+# Simple Message Relay
+The simple message relay is a basic implementation of a mediator/relay service which can be used for testing agent-to-agent comms.
+
+*This relay should never be used in production/public environments, as it intentionally lacks user authorization and data persistance for testing simplicitly purposes.*
+
+Like a mediator, an agent can provide their HTTP/s "user endpoint" to peers during DIDComm connection protocols. An agent can then poll to collect incoming messages that have been sent to their endpoint.
+
+The simplic design makes this service ideal for testing situations where a message-receiving endpoint is required by an agent, without the overhead of bootstrapping and connecting with an official Aries RFC based mediator.
+
+# Service Setup
+Within this directory, the service can be ran with `cargo`:
+```
+cargo run
+```
+
+Or from the aries-vcx repo base directory:
+```
+cargo run --bin simple_message_relay
+```
+
+This will start the relay serving on localhost port `8420`.
+
+## Public Endpoints
+The service can be exposed publicly by running the service on a machine with a public IP address exposed, ensuring that the service's port is exposed to incoming traffic.
+
+Alternatively, the port can easily be exposed using [ngrok](https://ngrok.com/).
+```
+ngrok http 8420
+```
+
+# Service Usage
+## Inform Peers
+To use the service as an aries agent, the agent should pick a unique ID (representing their user account) and encode it within their endpoint for the service:
+
+```
+{base_url}/send_user_message/{user_id}
+
+// e.g.
+
+http://localhost:8420/send_user_message/1234-1234-1234-1234
+```
+
+this "user endpoint" can then be used as the agent's `service_endpoint` in connection protocols, such as providing it into aries_vcx `Connection<Invitee, Invited>::send_request(.., service_endpoint: Url, ...)` API. This will inform the peer that they should send messages to this address.
+
+Peer agents can use this endpoint as if it was any other DIDComm http endpoint.
+
+## Collect Incoming Messages
+After a peer sends the agent a message to the endpoint, they can be collected by calling `GET` on the following endpoint for the user:
+
+```
+{base_url}/pop_user_message/{user_id}
+
+// e.g.
+
+http://localhost:8420/pop_user_message/1234-1234-1234-1234
+```
+
+The `user_id` should match the user_id selected in the [above section](#inform-peers).
+
+Calling this endpoint will return a body with the bytes of the oldest message in the relay message queue. Internally, the service will pop this message from the queue, such that the next call to the endpoint will return the next oldest message.
+
+If no message could be found, an empty body and a `NO CONTENT` status is returned.
+
+The bytes returned should be exactly as they were sent by the peer. As such, they should be able to be decrypted/unpacked and parsed as a DIDComm message, where they can then be passed into aries_vcx protocol handlers.
---
+++ tools/simple_message_relay/src/main.rs
@@ -0,0 +1,179 @@
+use std::{
+    collections::{HashMap, VecDeque},
+    sync::Mutex,
+};
+
+use actix_web::{
+    get, post,
+    web::{self, Bytes},
+    App, HttpResponse, HttpServer, Responder,
+};
+
+#[derive(Default)]
+struct UserMessages {
+    messages_by_user_id: Mutex<HashMap<String, VecDeque<Vec<u8>>>>,
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/pop_user_message/{user_id}")]
+async fn pop_user_message(path: web::Path<String>, data: web::Data<UserMessages>) -> impl Responder {
+    let user_id = path.into_inner();
+    let mut messages_by_user_id = data.messages_by_user_id.lock().unwrap();
+    let messages_for_user = messages_by_user_id.get_mut(&user_id);
+
+    let message_body = messages_for_user.and_then(|msgs| msgs.pop_front());
+
+    if let Some(body) = message_body {
+        return HttpResponse::Ok().body(body);
+    } else {
+        return HttpResponse::NoContent().into();
+    }
+}
+
+#[post("/send_user_message/{user_id}")]
+async fn send_user_message(path: web::Path<String>, body: Bytes, data: web::Data<UserMessages>) -> impl Responder {
+    let user_id = path.into_inner();
+
+    let body = body.to_vec();
+
+    let mut messages_by_user_id = data.messages_by_user_id.lock().unwrap();
+
+    let messages_for_user = messages_by_user_id.get_mut(&user_id);
+
+    if let Some(messages) = messages_for_user {
+        messages.push_back(body);
+    } else {
+        messages_by_user_id.insert(user_id, vec![body].into());
+    }
+
+    HttpResponse::Ok()
+}
+
+#[get("/status")]
+async fn status() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let user_messages = web::Data::new(UserMessages::default());
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(user_messages.clone())
+            .service(pop_user_message)
+            .service(send_user_message)
+            .service(status)
+    })
+    .bind(("0.0.0.0", 8420))?
+    .run()
+    .await
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{
+        body,
+        http::StatusCode,
+        test::{self, TestRequest},
+        web, App,
+    };
+
+    use crate::{pop_user_message, send_user_message, UserMessages};
+
+    fn pop_message_request(user_id: &str) -> TestRequest {
+        test::TestRequest::get().uri(&format!("/pop_user_message/{user_id}"))
+    }
+
+    fn send_message_request(user_id: &str, msg: &'static str) -> TestRequest {
+        test::TestRequest::post()
+            .uri(&format!("/send_user_message/{user_id}"))
+            .set_payload(msg)
+    }
+
+    #[actix_web::test]
+    async fn test_standard_user_flow() {
+        // assemble service
+        let user_messages = web::Data::new(UserMessages::default());
+
+        let app = test::init_service(
+            App::new()
+                .app_data(user_messages)
+                .service(send_user_message)
+                .service(pop_user_message),
+        )
+        .await;
+
+        let user_id = "user1";
+
+        // pop for unknown user == NO CONTENT
+        let pop_response = test::call_service(&app, pop_message_request(user_id).to_request()).await;
+        assert_eq!(pop_response.status(), StatusCode::NO_CONTENT);
+        let body = pop_response.into_body();
+        assert!(body::to_bytes(body).await.unwrap().is_empty());
+
+        // post a message in
+        let message = "hello world";
+        let req = send_message_request(user_id, message).to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+
+        // pop for known user == OK and body
+        let pop_response = test::call_service(&app, pop_message_request(user_id).to_request()).await;
+        assert_eq!(pop_response.status(), StatusCode::OK);
+        let body = pop_response.into_body();
+        assert_eq!(body::to_bytes(body).await.unwrap(), message.as_bytes());
+
+        // pop for no messages == NO CONTENT
+        let pop_response = test::call_service(&app, pop_message_request(user_id).to_request()).await;
+        assert_eq!(pop_response.status(), StatusCode::NO_CONTENT);
+        let body = pop_response.into_body();
+        assert!(body::to_bytes(body).await.unwrap().is_empty());
+    }
+
+    #[actix_web::test]
+    async fn test_multi_message_multi_user_flow() {
+        // assemble service
+        let user_messages = web::Data::new(UserMessages::default());
+
+        let app = test::init_service(
+            App::new()
+                .app_data(user_messages)
+                .service(send_user_message)
+                .service(pop_user_message),
+        )
+        .await;
+
+        let user_id1 = "user1";
+        let user_id2 = "user2";
+
+        let message1 = "message1";
+        let message2 = "message2";
+        let message3 = "message3";
+        let message4 = "message4";
+
+        // populate
+        test::call_service(&app, send_message_request(user_id1, message1).to_request()).await;
+        test::call_service(&app, send_message_request(user_id1, message2).to_request()).await;
+        test::call_service(&app, send_message_request(user_id2, message3).to_request()).await;
+        test::call_service(&app, send_message_request(user_id2, message4).to_request()).await;
+
+        // pop and check
+        let res = test::call_service(&app, pop_message_request(user_id1).to_request()).await;
+        assert_eq!(body::to_bytes(res.into_body()).await.unwrap(), message1.as_bytes());
+
+        let res = test::call_service(&app, pop_message_request(user_id2).to_request()).await;
+        assert_eq!(body::to_bytes(res.into_body()).await.unwrap(), message3.as_bytes());
+
+        let res = test::call_service(&app, pop_message_request(user_id1).to_request()).await;
+        assert_eq!(body::to_bytes(res.into_body()).await.unwrap(), message2.as_bytes());
+
+        let res = test::call_service(&app, pop_message_request(user_id2).to_request()).await;
+        assert_eq!(body::to_bytes(res.into_body()).await.unwrap(), message4.as_bytes());
+    }
+}
