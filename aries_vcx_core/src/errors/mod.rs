2f6f460d861731d2ee40138b32e8869099c10ce6
gmulhearn <gmulhearn@proton.me>
2023-08-17 19:00:16 +1000

    test
    Signed-off-by: gmulhearn <gmulhearn@proton.me>

--- aries_vcx_core/src/errors/mod.rs
+++ aries_vcx_core/src/errors/mod.rs
@@ -2,6 +2,7 @@ pub mod error;
 mod mapping_agency_client;
 #[cfg(any(feature = "modular_libs"))]
 mod mapping_credx;
+#[cfg(any(feature = "vdrtools_anoncreds", feature = "vdrtools_wallet"))]
 mod mapping_indy_api_types;
 mod mapping_indyvdr;
 #[cfg(feature = "vdr_proxy_ledger")]
