091529fddfd9b2cde3b80864b3b7f61f9720d8f8
Patrik <patrik.stas@absa.africa>
2023-08-26 20:57:21 +0200

    Replace openssl by crates num-bigint, sha2 (#317)
    * Replace openssl by crates 'num-bigint', 'sha2'
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    * Update dependencies
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>
    
    ---------
    
    Signed-off-by: Patrik Stas <patrik.stas@absa.africa>

--- aries_vcx/src/utils/openssl.rs
+++ aries_vcx/src/utils/openssl.rs
@@ -1,5 +1,5 @@
-use openssl::bn::BigNum;
-use openssl::sha::sha256;
+use num_bigint::BigUint;
+use sha2::{Digest, Sha256};
 
 use crate::errors::error::prelude::*;
 
@@ -7,18 +7,11 @@ pub fn encode(s: &str) -> VcxResult<String> {
     match s.parse::<u32>() {
         Ok(val) => Ok(val.to_string()),
         Err(_) => {
-            let hash = sha256(s.as_bytes());
-            let bignum = BigNum::from_slice(&hash).map_err(|err| {
-                AriesVcxError::from_msg(AriesVcxErrorKind::EncodeError, format!("Cannot encode string: {}", err))
-            })?;
-
-            let encoded = bignum
-                .to_dec_str()
-                .map_err(|err| {
-                    AriesVcxError::from_msg(AriesVcxErrorKind::EncodeError, format!("Cannot encode string: {}", err))
-                })?
-                .to_string();
-
+            let mut hasher = Sha256::new();
+            hasher.update(s.as_bytes());
+            let hash = hasher.finalize();
+            let bignum = BigUint::from_bytes_be(&hash.as_slice());
+            let encoded = bignum.to_str_radix(10);
             Ok(encoded)
         }
     }
