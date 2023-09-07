0bc8588759b536779ec6385b9d7a9afec4c7d6f8
Swapnil Tripathi <swapnil06.st@gmail.com>
2023-09-08 00:50:37 +0530

    add simple message relaying feature
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>

--- uniffi_aries_vcx/core/src/lib.rs
+++ uniffi_aries_vcx/core/src/lib.rs
@@ -5,8 +5,14 @@ pub mod errors;
 pub mod handlers;
 pub mod runtime;
 
-use aries_vcx::{
-    aries_vcx_core::wallet::indy::WalletConfig, protocols::connection::pairwise_info::PairwiseInfo,
+use crate::core::profile::*;
+use crate::core::utils::*;
+use crate::errors::error::*;
+use aries_vcx::aries_vcx_core::wallet::indy::WalletConfig;
+use aries_vcx::protocols::connection::pairwise_info::PairwiseInfo;
+use diddoc_legacy::{
+    aries::service::AriesService,
+    w3c::model::{Authentication, Ed25519PublicKey},
 };
 use handlers::connection::*;
 
--- uniffi_aries_vcx/demo/app/src/main/java/org/hyperledger/ariesvcx/HomeScreen.kt
+++ uniffi_aries_vcx/demo/app/src/main/java/org/hyperledger/ariesvcx/HomeScreen.kt
@@ -1,5 +1,6 @@
 package org.hyperledger.ariesvcx
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,7 +8,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -15,23 +21,70 @@ import androidx.navigation.NavHostController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+
 
 @Composable
-fun HomeScreen(navController: NavHostController, setProfileHolder: (ProfileHolder) -> Unit, profileHolder: ProfileHolder?, connection: Connection?) {
+fun HomeScreen(
+    navController: NavHostController,
+    setProfileHolder: (ProfileHolder) -> Unit,
+    profileHolder: ProfileHolder?,
+    connection: Connection?,
+    walletConfig: WalletConfig,
+    requested: Boolean,
+    httpClient: OkHttpClient
+) {
 
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
+    val TAG = "HomeScreen"
+    var intercept = true
 
-    val walletConfigState = WalletConfig(
-        walletName = "test_create_wallet_add_uuid_here",
-        walletKey = "8dvfYSt5d1taSd6yJdpjq4emkwsPDDLYxkNFysFD2cZY",
-        walletKeyDerivation = "RAW",
-        walletType = null,
-        storageConfig = null,
-        storageCredentials = null,
-        rekey = null,
-        rekeyDerivationMethod = null
-    )
+    var message by remember {
+        mutableStateOf("")
+    }
+
+    var unpackedMessage by remember {
+        mutableStateOf<String?>("")
+    }
+
+    val request = Request.Builder()
+        .url("https://03b7-27-57-116-96.ngrok-free.app/pop_user_message/${walletConfig.walletKey}")
+        .build()
+
+    LaunchedEffect(true) {
+        while (intercept && requested) {
+            httpClient.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.d(TAG, "onFailure: ${e.printStackTrace()}")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    response.use {
+                        if (response.code == 200) {
+                            message = response.body!!.string()
+
+                            unpackedMessage = String(
+                                unpackMessage(
+                                    profileHolder!!,
+                                    message.toByteArray().map { byte -> byte.toUByte() }
+                                ).map { it.toByte() }.toByteArray()
+                            )
+
+//                                connection.handleResponse(profileHolder!!)
+                            intercept = false
+                        }
+                        Log.d(TAG, "onResponse: ${message}")
+                    }
+                }
+            })
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -41,20 +94,24 @@ fun HomeScreen(navController: NavHostController, setProfileHolder: (ProfileHolde
         Button(
             enabled = (connection == null),
             onClick = {
-            scope.launch(Dispatchers.IO) {
-                val profile = newIndyProfile(walletConfigState)
-                setProfileHolder(profile)
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(context, "New Profile Created: ${profile.toString()}", Toast.LENGTH_SHORT).show()
+                scope.launch(Dispatchers.IO) {
+                    val profile = newIndyProfile(walletConfig)
+                    setProfileHolder(profile)
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(
+                            context,
+                            "New Profile Created: $profile",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            }
-        }) {
+            }) {
             Text(text = "New Indy Profile")
         }
         Button(enabled = (profileHolder != null && connection != null),
             onClick = {
-            navController.navigate(Destination.QRScan.route)
-        }) {
+                navController.navigate(Destination.QRScan.route)
+            }) {
             Text(text = "Scan QR Code")
         }
     }
