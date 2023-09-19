bb1b08a28bf2921dba8708553e4a8d611f597ffa
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-09-19 16:47:42 +0300

    Fixed did_parser crate lints
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- did_parser/src/utils/parse.rs
+++ did_parser/src/utils/parse.rs
@@ -97,7 +97,7 @@ fn parse_unqualified(did_url: &str) -> Result<(DidRange, Option<DidRange>, DidRa
         ));
     }
 
-    let id = did_url.split("#").next().unwrap_or(did_url);
+    let id = did_url.split('#').next().unwrap_or(did_url);
     shared_vcx::validation::did::validate_did(id)
         .map_err(|_| ParseError::InvalidInput("Unqualified DID failed validation"))?;
 
