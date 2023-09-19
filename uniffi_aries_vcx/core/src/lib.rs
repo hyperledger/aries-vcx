24e310f608e297c165fe75012e9f56bba3960744
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-09-19 16:22:03 +0300

    Fixed uniffi_aries_vcx lints
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- uniffi_aries_vcx/core/src/lib.rs
+++ uniffi_aries_vcx/core/src/lib.rs
@@ -5,12 +5,9 @@ pub mod errors;
 pub mod handlers;
 pub mod runtime;
 
-use crate::core::profile::*;
-use crate::errors::error::*;
-use aries_vcx::aries_vcx_core::wallet::indy::WalletConfig;
-use aries_vcx::protocols::connection::pairwise_info::PairwiseInfo;
-use diddoc_legacy::{
-    aries::service::AriesService,
-    w3c::model::{Authentication, Ed25519PublicKey},
+use aries_vcx::{
+    aries_vcx_core::wallet::indy::WalletConfig, protocols::connection::pairwise_info::PairwiseInfo,
 };
-use handlers::connection::{connection::*, *};
+use handlers::connection::*;
+
+use crate::{core::profile::*, errors::error::*};
