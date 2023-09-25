3f0d4a3284c570d1a4e99a46133eb991077e4e8a
Naian <126972030+nain-F49FF806@users.noreply.github.com>
2023-09-25 22:01:02 +0200

    Use UnpackMessage struct defined in aries vcx
    We can directly use centrally defined UnpackMessageOutput struct now, which this PR adds to aries_vcx_core
    
    This should keep the definition central rather than having to redefine it in multiple places.
    Allowing also single point of change.
    
    Signed-off-by: Naian <126972030+nain-F49FF806@users.noreply.github.com>

--- uniffi_aries_vcx/core/src/core/unpack_message.rs
+++ uniffi_aries_vcx/core/src/core/unpack_message.rs
@@ -1,28 +1,18 @@
 use std::sync::Arc;
 
-use serde::{Deserialize, Serialize};
+use aries_vcx::aries_vcx_core::wallet::structs_io::UnpackMessageOutput;
 
 use super::profile::ProfileHolder;
 use crate::{errors::error::VcxUniFFIResult, runtime::block_on};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
-pub struct UnpackMessage {
-    pub message: String,
-    pub recipient_verkey: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub sender_verkey: Option<String>,
-}
-
 pub fn unpack_message(
     profile_holder: Arc<ProfileHolder>,
     packed_msg: String,
-) -> VcxUniFFIResult<UnpackMessage> {
+) -> VcxUniFFIResult<UnpackMessageOutput> {
     block_on(async {
         let packed_bytes = packed_msg.as_bytes();
         let wallet = profile_holder.inner.inject_wallet();
-        let unpacked_bytes = wallet.unpack_message(packed_bytes).await?;
-        let unpacked_string = String::from_utf8(unpacked_bytes)?;
-        let unpacked_message = serde_json::from_str::<UnpackMessage>(&unpacked_string)?;
+        let unpacked_message = wallet.unpack_message(packed_bytes).await?;
         Ok(unpacked_message)
     })
 }
