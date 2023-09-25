f61f4a15ea0ac3463e910bbe9786936df2912a9d
Naian <126972030+nain-F49FF806@users.noreply.github.com>
2023-09-25 22:47:20 +0200

    patch into current name used in uniffi integration.
    pub type UnpackMessage = UnpackMessageOutput;
    
    Signed-off-by: Naian <126972030+nain-F49FF806@users.noreply.github.com>

--- uniffi_aries_vcx/core/src/core/unpack_message.rs
+++ uniffi_aries_vcx/core/src/core/unpack_message.rs
@@ -5,10 +5,12 @@ use aries_vcx::aries_vcx_core::wallet::structs_io::UnpackMessageOutput;
 use super::profile::ProfileHolder;
 use crate::{errors::error::VcxUniFFIResult, runtime::block_on};
 
+pub type UnpackMessage = UnpackMessageOutput;
+
 pub fn unpack_message(
     profile_holder: Arc<ProfileHolder>,
     packed_msg: String,
-) -> VcxUniFFIResult<UnpackMessageOutput> {
+) -> VcxUniFFIResult<UnpackMessage> {
     block_on(async {
         let packed_bytes = packed_msg.as_bytes();
         let wallet = profile_holder.inner.inject_wallet();
