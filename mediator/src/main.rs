6130b46d4d9f316a70d9bd24ead36b49fa07de19
Naian <126972030+nain-F49FF806@users.noreply.github.com>
2023-09-27 17:33:36 +0200

    Take endpoint root address from environment variable
    Signed-off-by: Naian <126972030+nain-F49FF806@users.noreply.github.com>

--- mediator/src/main.rs
+++ mediator/src/main.rs
@@ -1,9 +1,28 @@
+use log::info;
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-    let app_router = mediator::routes::build_router().await;
-    axum::Server::bind(&"0.0.0.0:8005".parse().unwrap())
-        .serve(app_router.into_make_service())
-        .await
-        .unwrap();
+    load_dot_env();
+    setup_logging();
+    let endpoint_root = std::env::var("ENDPOINT_ROOT").unwrap_or("0.0.0.0:8005".into());
+    info!("Endpoint root address {}", endpoint_root);
+    let app_router = mediator::routes::build_router(&endpoint_root).await;
+    axum::Server::bind(
+        &endpoint_root
+            .parse()
+            .expect("Pass an address to listen on like IP:PORT"),
+    )
+    .serve(app_router.into_make_service())
+    .await
+    .unwrap();
+}
+
+fn setup_logging() {
+    let env = env_logger::Env::default().default_filter_or("info");
+    env_logger::init_from_env(env);
+}
+
+fn load_dot_env() {
+    let _ = dotenvy::dotenv();
 }
