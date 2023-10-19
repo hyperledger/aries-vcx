05cea286515f1699073096c99c1626f768315cab
Naian <126972030+nain-F49FF806@users.noreply.github.com>
2023-10-19 14:38:32 +0200

    refactor: comment out unimplemented features
    Signed-off-by: Naian <126972030+nain-F49FF806@users.noreply.github.com>

--- agents/rust/mediator/mediation/src/storage/database/mod.rs
+++ agents/rust/mediator/mediation/src/storage/database/mod.rs
@@ -1,24 +1,25 @@
 // Copyright 2023 Naian G.
 // SPDX-License-Identifier: Apache-2.0
 
-#[cfg(any(
-    not(any(feature = "any_db", feature = "postgres_db", feature = "mysql_db")),
-    all(feature = "any_db", feature = "postgres_db", feature = "mysql_db"),
-    all(feature = "any_db", feature = "postgres_db"),
-    all(feature = "postgres_db", feature = "mysql_db"),
-    all(feature = "any_db", feature = "mysql_db")
-))]
-compile_error!("Pick any one of \"any_db\", \"postgresql_db\", \"mysql_db\" feature flags.");
+// unimplemented!()
+// #[cfg(any(
+//     not(any(feature = "any_db", feature = "postgres_db", feature = "mysql_db")),
+//     all(feature = "any_db", feature = "postgres_db", feature = "mysql_db"),
+//     all(feature = "any_db", feature = "postgres_db"),
+//     all(feature = "postgres_db", feature = "mysql_db"),
+//     all(feature = "any_db", feature = "mysql_db")
+// ))]
+// compile_error!("Pick any one of \"any_db\", \"postgresql_db\", \"mysql_db\" feature flags.");
 
-#[cfg(feature = "any_db")]
-mod any;
-#[cfg(feature = "any_db")]
-pub use any::get_db_pool;
+// #[cfg(feature = "any_db")]
+// mod any;
+// #[cfg(feature = "any_db")]
+// pub use any::get_db_pool;
 
-#[cfg(feature = "postgres_db")]
-mod postgres;
-#[cfg(feature = "postgres_db")]
-pub use postgres::get_db_pool;
+// #[cfg(feature = "postgres_db")]
+// mod postgres;
+// #[cfg(feature = "postgres_db")]
+// pub use postgres::get_db_pool;
 
 #[cfg(feature = "mysql_db")]
 mod mysql;
