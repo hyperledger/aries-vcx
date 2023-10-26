11867576e4dbddd67865b480ded55d5ea687dc90
Swapnil Tripathi <swapnil06.st@gmail.com>
2023-10-26 22:30:31 +0530

    migrate to Indy Vdr from MockLedger
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>

--- uniffi_aries_vcx/core/src/core/profile.rs
+++ uniffi_aries_vcx/core/src/core/profile.rs
@@ -3,8 +3,14 @@ use std::sync::Arc;
 use aries_vcx::{
     aries_vcx_core::{
         anoncreds::credx_anoncreds::IndyCredxAnonCreds,
-        ledger::base_ledger::TxnAuthrAgrmtOptions,
+        ledger::{
+            base_ledger::TxnAuthrAgrmtOptions,
+            indy_vdr_ledger::{indyvdr_build_ledger_read, IndyVdrLedgerRead},
+            request_submitter::vdr_ledger::{IndyVdrLedgerPool, IndyVdrSubmitter},
+            response_cacher::in_memory::{InMemoryResponseCacher, InMemoryResponseCacherConfig},
+        },
         wallet::indy::{wallet::create_and_open_wallet, IndySdkWallet, WalletConfig},
+        PoolConfig,
     },
     errors::error::{AriesVcxError, AriesVcxErrorKind, VcxResult},
 };
@@ -16,17 +22,12 @@ use crate::{errors::error::VcxUniFFIResult, runtime::block_on};
 pub struct UniffiProfile {
     wallet: IndySdkWallet,
     anoncreds: IndyCredxAnonCreds,
-    ledger_read: MockLedger,
-    ledger_write: MockLedger,
+    ledger_read: IndyVdrLedgerRead<IndyVdrSubmitter, InMemoryResponseCacher>,
 }
 
 impl UniffiProfile {
-    pub fn ledger_read(&self) -> &MockLedger {
+    pub fn ledger_read(&self) -> &IndyVdrLedgerRead<IndyVdrSubmitter, InMemoryResponseCacher> {
         &self.ledger_read
-    }
-
-    pub fn ledger_write(&self) -> &MockLedger {
-        &self.ledger_write
     }
 
     pub fn anoncreds(&self) -> &IndyCredxAnonCreds {
@@ -49,16 +50,25 @@ pub struct ProfileHolder {
     pub(crate) inner: UniffiProfile,
 }
 
-pub fn new_indy_profile(wallet_config: WalletConfig) -> VcxUniFFIResult<Arc<ProfileHolder>> {
+pub fn new_indy_profile(
+    wallet_config: WalletConfig,
+    genesis_file_path: String,
+) -> VcxUniFFIResult<Arc<ProfileHolder>> {
     block_on(async {
         let wh = create_and_open_wallet(&wallet_config).await?;
-
+        let indy_vdr_config = PoolConfig::default();
+        let cache_config = InMemoryResponseCacherConfig::builder()
+            .ttl(std::time::Duration::from_secs(60))
+            .capacity(1000)?
+            .build();
+        let ledger_pool = IndyVdrLedgerPool::new(genesis_file_path, indy_vdr_config, vec![])?;
+        let request_submitter = IndyVdrSubmitter::new(ledger_pool);
+        let ledger_read = indyvdr_build_ledger_read(request_submitter.clone(), cache_config)?;
         let wallet = IndySdkWallet::new(wh);
         let profile = UniffiProfile {
             anoncreds: IndyCredxAnonCreds,
             wallet,
-            ledger_read: MockLedger,
-            ledger_write: MockLedger,
+            ledger_read: ledger_read,
         };
 
         Ok(Arc::new(ProfileHolder { inner: profile }))
