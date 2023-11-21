2a8c51c6488cb466fa294a627a1c0e965f95606c
Swapnil Tripathi <swapnil06.st@gmail.com>
2023-11-21 13:33:06 +0530

    clean up
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>

--- uniffi_aries_vcx/demo/app/src/main/java/org/hyperledger/ariesvcx/Constants.kt
+++ uniffi_aries_vcx/demo/app/src/main/java/org/hyperledger/ariesvcx/Constants.kt
@@ -1,5 +1,5 @@
 package org.hyperledger.ariesvcx
 
 // Set your public IP address here, this endpoint will be used while communicating with the peer(agent).
-const val BASE_RELAY_ENDPOINT = "https://c0ea-2401-4900-5ae9-5941-eca0-812f-cb5f-4a86.ngrok-free.app";
+const val BASE_RELAY_ENDPOINT = "https://8dfb-2401-4900-8039-4a51-1e9a-23be-5e1b-f103.ngrok-free.app";
 const val RELAY_USER_ID = "demo-user-1";
--- uniffi_aries_vcx/demo/app/src/main/java/org/hyperledger/ariesvcx/HolderScreen.kt
+++ uniffi_aries_vcx/demo/app/src/main/java/org/hyperledger/ariesvcx/HolderScreen.kt
@@ -1,8 +1,5 @@
 package org.hyperledger.ariesvcx
 
-import android.net.Uri
-import android.util.Base64
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,18 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @Composable
 fun HolderScreen(
