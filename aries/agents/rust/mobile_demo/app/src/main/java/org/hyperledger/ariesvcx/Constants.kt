b7407cb7dbc094d6a6da3086aa50a64c6264da35
Swapnil Tripathi <swapnil06.st@gmail.com>
2023-11-22 21:35:04 +0530

    fix error implementation
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>

--- aries/agents/rust/mobile_demo/app/src/main/java/org/hyperledger/ariesvcx/Constants.kt
+++ aries/agents/rust/mobile_demo/app/src/main/java/org/hyperledger/ariesvcx/Constants.kt
@@ -1,5 +1,5 @@
 package org.hyperledger.ariesvcx
 
 // Set your public IP address here, this endpoint will be used while communicating with the peer(agent).
-const val BASE_RELAY_ENDPOINT = "https://c213-2401-4900-8129-59e9-2f9b-9f9d-38fa-a75.ngrok-free.app";
+const val BASE_RELAY_ENDPOINT = "https://fba3-2401-4900-5b09-4b4c-e180-b8b8-49a3-c1ee.ngrok-free.app";
 const val RELAY_USER_ID = "demo-user-1";
