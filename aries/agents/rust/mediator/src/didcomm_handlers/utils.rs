6a8e0f75c99deb13c467ac2e9ee3b42e38b616e0
Miroslav Kovar <miroslav.kovar@absa.africa>
2024-02-14 15:57:09 +0100

    Add justfile (#1125)
    * Add justfile
    
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- aries/agents/rust/mediator/src/didcomm_handlers/utils.rs
+++ aries/agents/rust/mediator/src/didcomm_handlers/utils.rs
@@ -1,8 +1,5 @@
 pub mod prelude {
-    pub use std::sync::Arc;
-
     pub use aries_vcx::utils::encryption_envelope::EncryptionEnvelope;
-    pub use aries_vcx_core::wallet::base_wallet::BaseWallet;
 
     pub use crate::{aries_agent::ArcAgent, persistence::MediatorPersistence, utils::prelude::*};
 }
