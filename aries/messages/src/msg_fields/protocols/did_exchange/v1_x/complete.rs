c9b6597f6ef9ee6f9a1d5051c07684428aa7556a
George Mulhearn <57472912+gmulhearn@users.noreply.github.com>
2024-07-03 13:39:01 +1000

    (refactor) small refactor to the new DIDExchange AnyXYZ wrappers (#1249)
    * remove deserialize from any wrappers
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * simply any wrappers
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    ---------
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    Co-authored-by: George Mulhearn <gmulhearn@anonyome.com>

--- aries/messages/src/msg_fields/protocols/did_exchange/v1_x/complete.rs
+++ aries/messages/src/msg_fields/protocols/did_exchange/v1_x/complete.rs
@@ -2,14 +2,10 @@ use serde::{Deserialize, Serialize};
 use shared::misc::serde_ignored::SerdeIgnored as NoContent;
 use typed_builder::TypedBuilder;
 
+use super::DidExchangeV1MessageVariant;
 use crate::{
     decorators::{thread::Thread, timing::Timing},
-    msg_fields::protocols::did_exchange::{
-        v1_0::DidExchangeV1_0, v1_1::DidExchangeV1_1, DidExchange,
-    },
     msg_parts::MsgParts,
-    msg_types::protocols::did_exchange::DidExchangeTypeV1,
-    AriesMessage,
 };
 
 /// Alias type for the shared DIDExchange v1.X complete message type.
@@ -29,41 +25,4 @@ pub struct CompleteDecorators {
     pub timing: Option<Timing>,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
-#[serde(untagged)]
-pub enum AnyComplete {
-    V1_0(Complete),
-    V1_1(Complete),
-}
-
-impl AnyComplete {
-    pub fn get_version(&self) -> DidExchangeTypeV1 {
-        match self {
-            AnyComplete::V1_0(_) => DidExchangeTypeV1::new_v1_0(),
-            AnyComplete::V1_1(_) => DidExchangeTypeV1::new_v1_1(),
-        }
-    }
-}
-
-impl AnyComplete {
-    pub fn into_inner(self) -> Complete {
-        match self {
-            AnyComplete::V1_0(r) | AnyComplete::V1_1(r) => r,
-        }
-    }
-
-    pub fn inner(&self) -> &Complete {
-        match self {
-            AnyComplete::V1_0(r) | AnyComplete::V1_1(r) => r,
-        }
-    }
-}
-
-impl From<AnyComplete> for AriesMessage {
-    fn from(value: AnyComplete) -> Self {
-        match value {
-            AnyComplete::V1_0(inner) => DidExchange::V1_0(DidExchangeV1_0::Complete(inner)).into(),
-            AnyComplete::V1_1(inner) => DidExchange::V1_1(DidExchangeV1_1::Complete(inner)).into(),
-        }
-    }
-}
+pub type AnyComplete = DidExchangeV1MessageVariant<Complete, Complete>;
--- aries/messages/src/msg_fields/protocols/did_exchange/v1_x/problem_report.rs
+++ aries/messages/src/msg_fields/protocols/did_exchange/v1_x/problem_report.rs
@@ -1,14 +1,10 @@
 use serde::{Deserialize, Serialize};
 use typed_builder::TypedBuilder;
 
+use super::DidExchangeV1MessageVariant;
 use crate::{
     decorators::{localization::MsgLocalization, thread::Thread, timing::Timing},
-    msg_fields::protocols::did_exchange::{
-        v1_0::DidExchangeV1_0, v1_1::DidExchangeV1_1, DidExchange,
-    },
     msg_parts::MsgParts,
-    msg_types::protocols::did_exchange::DidExchangeTypeV1,
-    AriesMessage,
 };
 
 /// Alias type for the shared DIDExchange v1.X problem report message type.
@@ -59,45 +55,4 @@ impl ProblemReportDecorators {
     }
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
-#[serde(untagged)]
-pub enum AnyProblemReport {
-    V1_0(ProblemReport),
-    V1_1(ProblemReport),
-}
-
-impl AnyProblemReport {
-    pub fn get_version(&self) -> DidExchangeTypeV1 {
-        match self {
-            AnyProblemReport::V1_0(_) => DidExchangeTypeV1::new_v1_0(),
-            AnyProblemReport::V1_1(_) => DidExchangeTypeV1::new_v1_1(),
-        }
-    }
-}
-
-impl AnyProblemReport {
-    pub fn into_inner(self) -> ProblemReport {
-        match self {
-            AnyProblemReport::V1_0(r) | AnyProblemReport::V1_1(r) => r,
-        }
-    }
-
-    pub fn inner(&self) -> &ProblemReport {
-        match self {
-            AnyProblemReport::V1_0(r) | AnyProblemReport::V1_1(r) => r,
-        }
-    }
-}
-
-impl From<AnyProblemReport> for AriesMessage {
-    fn from(value: AnyProblemReport) -> Self {
-        match value {
-            AnyProblemReport::V1_0(inner) => {
-                DidExchange::V1_0(DidExchangeV1_0::ProblemReport(inner)).into()
-            }
-            AnyProblemReport::V1_1(inner) => {
-                DidExchange::V1_1(DidExchangeV1_1::ProblemReport(inner)).into()
-            }
-        }
-    }
-}
+pub type AnyProblemReport = DidExchangeV1MessageVariant<ProblemReport, ProblemReport>;
