0e3bed00782ef6983dae11853d073253295f2bb0
George Mulhearn <57472912+gmulhearn@users.noreply.github.com>
2024-12-10 08:51:11 +1000

    (feat) Cheqd DID resolver #1300 (#1305)
    * try permissions in CI
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * new crate
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * resolution working
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * check in the proto types and have a standalone generator helper (rather than generate at build time)
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * regen lock
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * cheqd did parser
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * cheqd did url tests
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * re-gen lock
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * resolver system tests are working
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * run int tests in CI
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * clippy
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * resolution with contexts
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * finish some TODOs, and add doc metadata
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * update readme
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * try bumping rust ver on vdrproxy
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * lock auto update
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * readme updates
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * remove some debugs
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * more error info
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * updated hyper in did:web. fixes clients
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * fix up tls. works on android
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    * some cleaning
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    
    ---------
    
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>
    Co-authored-by: George Mulhearn <gmulhearn@anonyome.com>

--- did_core/did_doc/src/schema/mod.rs
+++ did_core/did_doc/src/schema/mod.rs
@@ -3,3 +3,19 @@ pub mod service;
 pub mod types;
 pub mod utils;
 pub mod verification_method;
+
+/// Module of commonly used DID-related JSON-LD contexts
+pub mod contexts {
+    pub const W3C_DID_V1: &str = "https://www.w3.org/ns/did/v1";
+    pub const W3C_SUITE_ED25519_2020: &str = "https://w3id.org/security/suites/ed25519-2020/v1";
+    pub const W3C_SUITE_ED25519_2018: &str = "https://w3id.org/security/suites/ed25519-2018/v1";
+    pub const W3C_SUITE_JWS_2020: &str = "https://w3id.org/security/suites/jws-2020/v1";
+    pub const W3C_SUITE_SECP256K1_2019: &str = "https://w3id.org/security/suites/secp256k1-2019/v1";
+    pub const W3C_BBS_V1: &str = "https://w3id.org/security/bbs/v1";
+    pub const W3C_PGP_V1: &str = "https://w3id.org/pgp/v1";
+    pub const W3C_SUITE_X25519_2019: &str = "https://w3id.org/security/suites/x25519-2019/v1";
+    pub const W3C_SUITE_X25519_2020: &str = "https://w3id.org/security/suites/x25519-2020/v1";
+    pub const W3C_SUITE_SECP259K1_RECOVERY_2020: &str =
+        "https://w3id.org/security/suites/secp256k1recovery-2020/v2";
+    pub const W3C_MULTIKEY_V1: &str = "https://w3id.org/security/multikey/v1";
+}
